3cb64e09df71ef87f6bb8bd9a4044754366b3df3
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 00:00:57 +0530

    .
    .

---
+++ NumTriangle2/src/NumTriangle2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class NumTriangle2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        System.out.println("Enter the number");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        for (int i =1;i<=num;i++){
+            for (int j=1; j<i+1; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
+        for (int i =num-1;i>=0;i--){
+            for (int j=1; j<i+1; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
