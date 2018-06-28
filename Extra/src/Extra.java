609b821e8d0effbb095965ae03a5c8e1808eec1b
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 01:58:53 +0530

    .
    .

--- Extra/src/Extra.java
+++ Extra/src/Extra.java
@@ -1,29 +1,18 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 public class Extra {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Number");
-        int num =0;
-        try{
-            num = input.nextInt();
-        }
-        catch (InputMismatchException ex){
-            System.out.println("Only digits are allowed");
-        }
-        catch (Exception ex){
-            System.out.println(ex);
-        }
-        calculate(num);
+        int num = input.nextInt();
+        uglyNum(num);
     }
-    private static void calculate(int n){
-        int sum = 0;
-        while(n>0){
-            sum += n%10;
-            n=n/10;
+    static void uglyNum(int n){
+        if(n%2==0 || n%3==0 || n%5==0){
+            System.out.println("Yes it is a an Ugly Number");
         }
-        System.out.println("Sum = "+ sum);
-
+        else{
+            System.out.println("No");
+        }
     }
 }
