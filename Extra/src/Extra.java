d463d2324664fc14119ccb183b485c580ecf71e3
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 00:08:31 +0530

    .
    .

---
+++ Extra/src/Extra.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Extra {
+
+    public static void main(String[] args) {
+        int sum = 0, k=1;
+        System.out.println("Enter the number");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        for (int i =1;i<=num;i++){
+            for (int j=1; j<i; j++){
+                System.out.print(k++ +" " );
+            }
+            System.out.println("");
+        }
+        for (int i =num;i>=0;i--){
+            for (int j=1; j<i; j++){
+                System.out.print(k++ +" " );
+            }
+            System.out.println("");
+        }
+    }
+}
