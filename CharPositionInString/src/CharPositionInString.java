e044e45910149c95ce452db88ba5c659201e2086
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 01:24:09 +0530

    .
    .

--- CharPositionInString/src/CharPositionInString.java
+++ CharPositionInString/src/CharPositionInString.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Extra {
+public class CharPositionInString {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
