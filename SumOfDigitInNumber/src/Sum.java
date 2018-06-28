1b7621859d79096f3a5d4930f9355630465b1c45
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 01:57:22 +0530

    .
    .

--- SumOfDigitInNumber/src/Sum.java
+++ SumOfDigitInNumber/src/Sum.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
-public class Extra {
+public class Sum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Number");
