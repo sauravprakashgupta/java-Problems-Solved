5df6efe5e63a061f4a7a8bceacdaac2285e56009
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-29 00:57:26 +0530

    .
    .

--- RepeatedNumbers/src/RepeatedNumber.java
+++ RepeatedNumbers/src/RepeatedNumber.java
@@ -2,7 +2,7 @@ import com.sun.deploy.util.ArrayUtil;
 
 import java.util.Scanner;
 import java.util.Arrays;
-public class Extra {
+public class RepeatedNumber {
 
     public static void main(String[] args) {
         int[] intArr = {12,43,65,24,64,24,677,95,11,12,677,88,64};
@@ -10,8 +10,8 @@ public class Extra {
 
         for(int x=0;x<intArr.length;x++){
             for (int y=1;y<intArr.length; y++){
-                if (intArr[x] == intArr[y] && x != y){
-                    System.out.println(intArr[y] + " is present at position "+ (x+1));
+                if (intArr[x] == intArr[y] && x != y && y>x){
+                    System.out.println(intArr[x] + " is present at position "+ (x+1));
                 }
             }
         }
