eec1db908af1665912cafc52b8bd7f2fbe756002
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-30 02:53:43 +0530

    Adding ComboBox with Images
    Images can be switched based on selection made in comboBox

---
+++ GUI_jComboBox/src/MainClass.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Gui_jComboBox comboObject = new Gui_jComboBox();
+        comboObject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        comboObject.setSize(300,200);
+        comboObject.setVisible(true);
+    }
+}
