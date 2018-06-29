c387a930f7c2ca045218c74af65200bbe5c445aa
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-30 03:27:08 +0530

    Adding Multiple Selection list
    Leftpane to right pane based on value selected and a move button

---
+++ GUI_MultipleSelect/src/MainClass.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MultipleSelect myObj = new MultipleSelect();
+        myObj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myObj.setSize(300,200);
+        myObj.setVisible(true);
+    }
+}
