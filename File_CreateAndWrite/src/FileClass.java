5b3c3159c0777030143552d85377d9112e9871ee
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-30 14:45:28 +0530

    File creation, write, read and close
    .

--- File_CreateAndWrite/src/FileClass.java
+++ File_CreateAndWrite/src/FileClass.java
@@ -1,8 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Formatter;
+import java.util.Scanner;
 
 public class FileClass {
     private String filePath;
--- File_CreateAndWrite/src/MainClass.java
+++ File_CreateAndWrite/src/MainClass.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 public class MainClass {
     public static void main(String[] args) throws IOException {
         FileClass file = new FileClass();
-        file.createFile("C:\\Users\\SouravG\\Downloads\\saurav\\testFoleder\\testDocNew.txt");
+//        file.createFile("C:\\Users\\SouravG\\Downloads\\saurav\\testFoleder\\testDocNew.txt");
+        file.createFile("testDoc.txt");
         file.addRecords(" This is the content of my file, Line 1. \n This is the content of my file, Line 2. \n This is the content of my file, Line 3.");
         file.closeFile();
 
-        file.readRecords("C:\\Users\\SouravG\\Downloads\\saurav\\testFoleder\\testDocNew.txt");
-
+//        file.readRecords("C:\\Users\\SouravG\\Downloads\\saurav\\testFoleder\\testDocNew.txt");
+        file.readRecords("testDoc.txt");
     }
 }
